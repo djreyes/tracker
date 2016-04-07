29f670624e18691a879caaba9697d3a717b7cdc5
Jeremy Reyes <djreyes@gmail.com>
2016-04-07 11:55:51 -0700

    Update priority and owner handling in views

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :owner, foreign_key: :assigned_to_id, class_name: 'User'
+
+  delegate :name, :email, to: :owner, prefix: true
+
+  validates :priority, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   it { is_expected.to belong_to(:owner) }
+  it { is_expected.to validate_numericality_of(:priority) }
 end
